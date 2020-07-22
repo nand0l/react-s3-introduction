407cb02d275eb20e0615d7fc638ae8ee58991be4
nand0l <36447266+nand0l@users.noreply.github.com>
2020-07-22 20:25:00 +0200

    included build

---
+++ build/precache-manifest.f847c9de78fadbd213439f1d7d67744d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "192c8ccb6eeacd533928f1e124d1af0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d5d0bfe769d8e1daade",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c6cbc6af6ffbb11c7891",
+    "url": "/static/js/2.4294c854.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4294c854.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d5d0bfe769d8e1daade",
+    "url": "/static/js/main.e44c6099.chunk.js"
+  },
+  {
+    "revision": "63d298350039f8a36212",
+    "url": "/static/js/runtime-main.342c6ca9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
